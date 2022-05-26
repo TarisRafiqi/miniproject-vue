3ac407f90547bc7d7bbb2c9aeaf997c5b300681c
TarisRafiqi <taris.izatri18@student.uisi.ac.id>
2022-05-26 14:54:54 +0700

    test

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,19 @@
 <template>
   <v-app>
-    <v-toolbar dark height="50">
-      <v-toolbar-side-icon></v-toolbar-side-icon>
-      <v-toolbar-title>Kalkulator BMI</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn flat to="/">Halaman 1</v-btn>
-        <v-btn flat to="/about">Halaman 2</v-btn>
-        <v-btn flat to="/halamanTiga">Halaman 3</v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
-
     <v-content>
+      <home-view/>
       <router-view/>
-      <HelloWorld/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomeView from './components/Header'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{
+HomeView
   },
 
   data: () => ({
---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-toolbar dark height="50">
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title>Kalkulator BMI</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat to="/">Halaman 1</v-btn>
+        <v-btn flat to="/about">Halaman 2</v-btn>
+        <v-btn flat to="/halamanTiga">Halaman 3</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+</template>
+
+<script>
+export default {
+name:"HeaderComp"
+}
+</script>
+
+<style>
+
+</style>
