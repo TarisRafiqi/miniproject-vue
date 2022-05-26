3e596dea3f6552cd5352bd01ea8948c10b940568
TarisRafiqi <taris.izatri18@student.uisi.ac.id>
2022-05-26 22:46:59 +0700

    Navbar fix sementara

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,30 @@
 <template>
-<v-card class='m-5'>
-  <v-card-title>Kalkulator BMI</v-card-title>
 
-  <v-card-text>Kalori merupakan nilai atau satuan yang menunjukkan seberapa banyak jumlah energi yang bisa diperoleh dari makanan dan minuman. Gunakan kalkulator kalori ini untuk menentukan berapa kebutuhan kalori harian anda.</v-card-text>
+  <v-card color="#8BC34A" class="mx-10 my-5 px-5 py-5" dark>
+    
 
-  <v-card-actions>
-    <v-btn color="success">Hitung Sekarang</v-btn>
-  </v-card-actions>
-</v-card>
+    <v-layout row>
+      <v-flex xs6 md6 lg8>
+        <v-card-title>Kalkulator BMI</v-card-title>
+        <v-card-text>Kalori merupakan nilai atau satuan yang menunjukkan seberapa banyak jumlah energi yang bisa diperoleh dari makanan dan minuman. Gunakan kalkulator kalori ini untuk menentukan berapa kebutuhan kalori harian anda.</v-card-text>
+
+        <v-card-actions>
+        <v-btn class="white black--text">Hitung Sekarang</v-btn>
+      </v-card-actions>
+
+      </v-flex>
+
+      <v-flex xs6 md6 lg4>
+        <v-img
+          :src="require('../assets/gauge-Logo.png')"
+          contain
+          height="200">
+          </v-img>
+      </v-flex>
+
+    </v-layout>
+  </v-card>
+
 
 </template>
 
